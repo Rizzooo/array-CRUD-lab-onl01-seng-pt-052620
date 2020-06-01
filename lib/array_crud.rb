d59c56901df81b0e384d35706ceef73fafd7131d
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-01 16:13:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  #{array} << #{element}
-  add_element_to_end_of_array("array", "Item Four")
+  
 end
 
 def add_element_to_start_of_array(array, element)
