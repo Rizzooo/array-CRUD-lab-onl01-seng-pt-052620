2336b80bfc86984b220afb6a9df97e19b2c733d8
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-01 16:29:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  #{array}.pop()
+  last_gone = #{array}.pop()
+  last_gone
 end
 
 def remove_element_from_start_of_array(array)
