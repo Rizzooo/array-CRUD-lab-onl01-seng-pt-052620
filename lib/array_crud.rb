6d98d4f65ad1c7afe2af55e9bfedb9546f4d325c
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-01 16:06:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   #{array} << #{element}
-  add_element_to_end_of_array("boring_array", "Item Four")
+  add_element_to_end_of_array(boring_array, "Item Four")
 end
 
 def add_element_to_start_of_array(array, element)
